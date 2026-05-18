edcc786d6e4d4a6c5d2ad592401fb1136f91c0f9
Piyush Singh <114435992+piyushk1302@users.noreply.github.com>
2026-05-18 22:30:08 +0530

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,1 +1,10 @@
+@rem ##########################################################################
+@rem
+@rem  Gradle startup script for Windows
+@rem
+@rem ##########################################################################
 
+@echo off
+set DIR=%~dp0
+
+java -classpath "%DIR%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
